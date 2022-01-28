a8b64216593ec3087d6f1e86f146aae307a94905
Raúl Francés <raulfrancesm1997@gmail.com>
2022-01-28 22:37:24 +0100

    Ahora resta

--- Assets/Scripts/WorkController.cs
+++ Assets/Scripts/WorkController.cs
@@ -4,6 +4,11 @@ public class WorkController : MonoBehaviour
 {
     public bool isActive = false;
     public float efficiencyLossPerSecond = 0.10f;
+    public GameObject[] rooms;
+    private void Awake()
+    {
+        rooms = Resources.LoadAll<GameObject>("Rooms");
+    }
 
     private void Update()
     {
@@ -17,10 +22,16 @@ public class WorkController : MonoBehaviour
     public void Activate()
     {
         isActive = true;
+
     }
 
     public void Deactivate()
     {
         isActive = false;
+    }
+
+    public void LoadRoom()
+    {
+
     }
 }
