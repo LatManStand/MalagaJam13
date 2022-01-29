91f64a05f1720b5cdda477789b3ce9bdef179633
Raúl Francés <raulfrancesm1997@gmail.com>
2022-01-29 14:21:56 +0100

    Cosas de musica

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,13 @@ using UnityEngine.UI;
 
 public class MusicManager : MonoBehaviour
 {
+    public enum MusicType
+    {
+        menu,
+        work,
+        relax
+    }
+
     public const string MASTER_KEY = "MasterVolume";
     public const string AMBIENT_KEY = "AmbientVolume";
     public const string MUSIC_KEY = "MusicVolume";
@@ -30,6 +38,7 @@ public class MusicManager : MonoBehaviour
         if (instance == null)
         {
             instance = this;
+            SetSlidersValues();
             DontDestroyOnLoad(gameObject);
         }
         else if (instance != this)
@@ -38,27 +47,60 @@ public class MusicManager : MonoBehaviour
         }
     }
 
+    private void SetSlidersValues()
+    {
+        master.value = PlayerPrefs.GetFloat(MASTER_KEY, 1.0f);
+        ambient.value = PlayerPrefs.GetFloat(AMBIENT_KEY, 1.0f);
+        music.value = PlayerPrefs.GetFloat(MUSIC_KEY, 1.0f);
+        sfx.value = PlayerPrefs.GetFloat(SFX_KEY, 1.0f);
+
+
+        audioMixer.SetFloat(MASTER_KEY, Mathf.Log10(master.value) * 20);
+        audioMixer.SetFloat(AMBIENT_KEY, Mathf.Log10(ambient.value) * 20);
+        audioMixer.SetFloat(MUSIC_KEY, Mathf.Log10(music.value) * 20);
+        audioMixer.SetFloat(SFX_KEY, Mathf.Log10(sfx.value) * 20);
+    }
+
     public void ChangeVolume(SoundSlider.MixerType mixerType, float ammount)
     {
         switch (mixerType)
         {
             case SoundSlider.MixerType.Master:
                 audioMixer.SetFloat(MASTER_KEY, Mathf.Log10(ammount) * 20);
+                PlayerPrefs.SetFloat(MASTER_KEY, ammount);
                 break;
             case SoundSlider.MixerType.Ambient:
                 audioMixer.SetFloat(AMBIENT_KEY, Mathf.Log10(ammount) * 20);
+                PlayerPrefs.SetFloat(AMBIENT_KEY, ammount);
                 break;
             case SoundSlider.MixerType.Music:
                 audioMixer.SetFloat(MUSIC_KEY, Mathf.Log10(ammount) * 20);
+                PlayerPrefs.SetFloat(MUSIC_KEY, ammount);
                 break;
             case SoundSlider.MixerType.SFX:
                 audioMixer.SetFloat(SFX_KEY, Mathf.Log10(ammount) * 20);
+                PlayerPrefs.SetFloat(SFX_KEY, ammount);
                 break;
         }
     }
 
-    public void ChangeMusic()
+    public void ChangeMusic(MusicType musicType)
     {
+        menuMusic.Pause();
+        workMusic.Pause();
+        relaxMusic.Pause();
 
+        switch (musicType)
+        {
+            case MusicType.menu:
+                menuMusic.Play();
+                break;
+            case MusicType.work:
+                workMusic.Play();
+                break;
+            case MusicType.relax:
+                relaxMusic.Play();
+                break;
+        }
     }
 }
